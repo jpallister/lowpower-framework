38b25f63c3c7bb6c55eded22c3de479d173ff404
James Pallister <jp8257@bristol.ac.uk>
2012-08-06 17:39:53 +0100

    Updated test.py to test a subset of the flags

--- test.py
+++ test.py
@@ -25,18 +25,39 @@ options = [
 ]
 
 tm = benchmark.TestManager(optionsfile="options-4.7.1.csv")
+
+flags = ["-fauto-inc-dec", "-fcombine-stack-adjustments",
+        "-fcompare-elim", "-fcprop-registers",
+        "-fdce", "-fdefer-pop",
+        "-fdelayed-branch", "-fdse", "-fguess-branch-probability",
+        "-fif-conversion", "-fif-conversion2",
+        "-finline-functions-called-once", "-fipa-profile",
+        "-fipa-pure-const", "-fipa-reference",
+        "-fmerge-constants", "-fmove-loop-invariants",
+        "-fomit-frame-pointer", "-fshrink-wrap",
+        "-fsplit-wide-types", "-ftree-bit-ccp",
+        "-ftree-ccp", "-ftree-ch",
+        "-ftree-copy-prop", "-ftree-copyrename",
+        "-ftree-dce", "-ftree-dominator-opts",
+        "-ftree-dse", "-ftree-forwprop",
+        "-ftree-fre", "-ftree-loop-optimize",
+        "-ftree-phiprop", "-ftree-pta",
+        "-ftree-reassoc", "-ftree-sink",
+        "-ftree-sra", "-ftree-ter"]
+
+
+tm.useOptionSubset(flags)
 #tm = benchmark.TestManager(options)
 
-m = fracfact.FactorialMatrix(len(tm.options))
+m = fracfact.FactorialMatrix(len(flags))
 m.combinationFactorial(1)
 
 comb_mat = m.getTrueFalse()[1:]
 
 for i, comb in enumerate(comb_mat):
     test = tm.createTest(comb)
-    test.compile()
-    test.run()
-    r = test.get_result()
+    test.loadOrRun()
+    r = test.getResult()
 
     m.addResult(i, r)
 
